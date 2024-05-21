469df102b2b518eeb35a3ace814e143f4175db99
Connor Walton <46007308+Ilovetoocode@users.noreply.github.com>
2024-05-21 08:18:55 -0600

    Hope this works!

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -1,6 +1,10 @@
-extends Node
+extends Node2D
+@onready var player := $"."
 
-
+func _physics_process(delta):
+	var speed: float = 0.01 # put wanted speed here
+	look_at(player.global_position)
+	self.position = lerp(self.position,player.global_position,speed)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
