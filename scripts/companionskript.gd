7474cdfa5aa3e49b1c73e7b7f979088287505f82
KonradBu <konradburgi1@gmail.com>
2024-05-23 17:20:51 +0200

    start companionskript

--- scripts/companionskript.gd
+++ scripts/companionskript.gd
@@ -3,7 +3,34 @@ extends CharacterBody2D
 @export var companionSpeed = 200
 @export var health = 20
 
-enum companionAbilities{shout, sprint}
+enum armParts {default, shout, posion}
+enum legParts {default, sprint, stomp}
+enum coreParts {default, heavy, light}
+
+var currentArm = armParts.default
+var currentLeg = legParts.default
+var currentCore = coreParts.default
 
 func _physics_process(delta):
-	pass
+	match currentArm:
+		armParts.shout:
+			pass
+		armParts.posion:
+			pass
+		armParts.default:
+			pass
+	match (currentLeg):
+		legParts.default:
+			pass
+		legParts.stomp:
+			pass
+		legParts.sprint:
+			pass
+	match (currentCore):
+		coreParts.heavy:
+			pass
+		coreParts.light:
+			pass
+		coreParts.default:
+			pass
+			
