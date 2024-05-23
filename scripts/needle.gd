08914e5b84463edcedf1b2d0a6a498284e33f0fd
LucIsUncool <lucartsymations123@gmail.com>
2024-05-23 23:54:34 +0800

    needleAnimations

--- scripts/needle.gd
+++ scripts/needle.gd
@@ -1,13 +1,19 @@
 extends CharacterBody2D
 @export var needleSpeed = 4000
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var ray_cast_2d = $RayCast2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var mousePosition = get_viewport().get_mouse_position()
 	look_at(mousePosition)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-	
+
 func _physics_process(delta):
 	position += transform.x * needleSpeed * delta
+	animated_sprite_2d.play("Spin")
+	
+	if ray_cast_2d.is_colliding() == true:
+		velocity.x = 0
+		velocity.y = 0
+		
+
