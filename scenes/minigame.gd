34854ee3a1085df2c8a20e552cc1349ebb655ffd
Connor Walton <46007308+Ilovetoocode@users.noreply.github.com>
2024-05-23 08:39:49 -0600

    Generation code is starting!

---
+++ scenes/minigame.gd
@@ -0,0 +1,11 @@
+extends Node2D
+@onready var tilemap = $TileMap
+const initial_width = 37
+const initial_height = 21
+var map_width = initial_width
+var map_height = initial_height
+var map_offset = 4 
+const Safe_areas =1
+const Bad_areas = 0
+const Collection_areas = 3
+const Tile_layer = 0
