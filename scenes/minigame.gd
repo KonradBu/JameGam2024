19835dd28bab66e71c2553f48080bf5f2cf02503
Connor Walton <46007308+Ilovetoocode@users.noreply.github.com>
2024-05-23 09:06:35 -0600

    Starting this again

--- scenes/minigame.gd
+++ scenes/minigame.gd
@@ -1,11 +1,11 @@
 extends Node2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+@onready var tilemap = $TileMap
+const initial_width = 37
+const initial_height = 21
+var map_width = initial_width
+var map_height = initial_height
+var map_offset = 4
+const Fail_tile_id = 0
+const Safe_tile_id = 1
+const Win_tile_id = 3
+const tile_layer=0
