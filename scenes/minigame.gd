f3a5c90e5e9dc1793b6c254037dfe726a3c8b390
Connor Walton <46007308+Ilovetoocode@users.noreply.github.com>
2024-05-23 08:51:14 -0600

    Thing is not working

--- scenes/minigame.gd
+++ scenes/minigame.gd
@@ -9,3 +9,11 @@ const Safe_areas =1
 const Bad_areas = 0
 const Collection_areas = 3
 const Tile_layer = 0
+func _ready():
+		generate_fails()
+
+func generate_fails():
+	for x in range(map_width):
+		for y in range(map_height):
+			if x == 0 or x == map_width - 1 or y == 0 or y == map_height - 1:
+				tilemap.set_cell(Tile_layer, Vector2i(x, y + map_offset), Bad_areas, Vector2i(0, 0), 0)
